c4297f01e36193c5a92727179cac988d733d87ce
Lucas Oliveira <lucasoft.dev@outlook.com>
2024-06-15 19:55:13 -0300

    saving user credentials

--- lib/stores/register_store.dart
+++ lib/stores/register_store.dart
@@ -1,10 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-// import 'package:crypto/crypto.dart';
+import 'package:crypto/crypto.dart';
 import 'dart:convert'; // for the utf8.encode method
-// import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:mobx/mobx.dart';
+
+import '../utils/helper.dart';
 
 part 'register_store.g.dart';
 
@@ -100,72 +104,22 @@ abstract class RegisterStoreBase with Store {
 
   @action
   Future<void> register() async {
-    // Validate the username and password
-    // if (username.isEmpty || password.isEmpty || confirmPassword.isEmpty) {
-    //   throw Exception(
-    //       'Username, password and confirmation password must not be empty');
-    // }
-    //
-    // if (password != confirmPassword) {
-    //   throw Exception('Password and confirmation password must match');
-    // }
+    var salt = Helper.generateSalt(32);
+    var saltedPassword = password! + salt;
+    var bytes = utf8.encode(saltedPassword);
+    var digest = sha256.convert(bytes);
 
-    // Hash the password
-    // var bytes = utf8.encode(password); // data being hashed
-    // var digest = sha256.convert(bytes);
-
-    // Store the username and hashed password in local storage
-    // SharedPreferences prefs = await SharedPreferences.getInstance();
-    // await prefs.setString('username', username);
-    // await prefs.setString('password', digest.toString());
-
-    // If the registration was not successful, throw an error
-    // if (!success) {
-    //   throw Exception('Registration failed');
-    // }
+    try {
+      await _saveCredentials(digest, salt);
+    } catch (e) {
+      throw Exception('Registration failed');
+    }
   }
 
-// final TextEditingController loginController = TextEditingController();
-// final TextEditingController passwordController = TextEditingController();
-// final TextEditingController confirmPasswordController =
-//     TextEditingController();
-//
-// String? get loginError {
-//   if (loginController.text.isEmpty) {
-//     return 'Login cannot be empty';
-//   }
-//   return null;
-// }
-//
-// bool get isFormValid {
-//   return loginController.text.isNotEmpty &&
-//       passwordController.text.isNotEmpty &&
-//       confirmPasswordController.text.isNotEmpty;
-// }
-
-// Future<void> register() async {
-// Validate the username and password
-// if (username.isEmpty || password.isEmpty || confirmPassword.isEmpty) {
-//   throw Exception(
-//       'Username, password and confirmation password must not be empty');
-// }
-//
-// if (password != confirmPassword) {
-//   throw Exception('Password and confirmation password must match');
-// }
-
-// Hash the password
-// var bytes = utf8.encode(password); // data being hashed
-// var digest = sha256.convert(bytes);
-
-// Store the username and hashed password in local storage
-// SharedPreferences prefs = await SharedPreferences.getInstance();
-// await prefs.setString('username', username);
-// await prefs.setString('password', digest.toString());
-
-// If the registration was not successful, throw an error
-// if (!success) {
-//   throw Exception('Registration failed');
-// }
-// }
-}
+  Future<void> _saveCredentials(Digest digest, String salt) async {
+    // Store the username and hashed password in local storage
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('username', login!);
+    await prefs.setString('password', digest.toString());
+    await prefs.setString('salt', salt);
+  }
